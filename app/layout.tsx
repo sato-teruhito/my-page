ce846cfa4a4d5c32c2f4bef7c83b16a2b15f36f8
teru <s266126@wakayama-u.ac.jp>
2025-05-31 05:40:23 +0900

    remove:  Theme Provide

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "./components/layouts/header/Header"
 import Footer from "./components/layouts/footer/Footer"
-import ThemeProvider from "./theme/theme-provider"
+//import ThemeProvider from "./theme/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
